28aacf53537bba8d28f1ae3fb01ed301d21ccde1
EquesAuratus <illia.vysochyn@nure.ua>
2023-10-24 17:38:01 +0300

    feat(registration):html is being worked on

--- avpz/src/App.tsx
+++ avpz/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'react-notifications/lib/notifications.css';
-import Calendar from 'react-calendar'
 import {Routes, Route} from "react-router-dom";
 import MainPage from "./Pages/MainPage";
+import Registration from "./Pages/Registration";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<MainPage />}/>
+            <Route path="/signup" element={<Registration />}/>
         </Routes>
     );
 }
--- avpz/src/Pages/Registration.tsx
+++ avpz/src/Pages/Registration.tsx
@@ -1,6 +1,235 @@
+import {useState} from "react";
+import {CheckPassword} from "../Utilities/CheckPassword";
+import "../Styles/Registration.css"
+
 function Registration() {
-    return <>
-    </>
+    const [companyName, setCompanyName] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [LastName, setLastName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState("");
+    const [password1, setPassword1] = useState("");
+    const [password2, setPassword2] = useState("");
+    const [showPassword1, setShowPassword1] = useState(false);
+    const [showPassword2, setShowPassword2] = useState(false);
+    const [errorCompanyName, setErrorCompanyName] = useState("");
+    const [errorFirstName, setErrorFirstName] = useState("");
+    const [errorLastName, setErrorLastName] = useState("")
+    const [errorEmail, setErrorEmail] = useState("");
+    const [errorPassword1, setErrorPassword1] = useState("");
+    const [errorPassword2, setErrorPassword2] = useState("");
+    const [errorPhoneNumber, setErrorPhoneNumber] = useState('');
+
+    function RegistrationConfirming(email: string, pas1: string, pas2: string) {
+
+    }
+
+    return (
+        <body>
+        <p>Header</p>
+        <div>
+            <h2>Create account</h2>
+            <form className="space-y-4" action="#" method="POST" onSubmit={(e) => {
+                e.preventDefault();
+                RegistrationConfirming(email, password1, password2)
+            }}>
+                <div>
+                    <label htmlFor="companyName">
+                        Company name
+                    </label>
+                    <label>*</label>
+                    <input
+                        onChange={(e) => {
+                            setCompanyName(e.target.value)
+                            if (!RegExp("^[a-zA-Z0-9!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~]{1,20}$").test(companyName))
+                                setErrorCompanyName("Incorrect Company Name")
+                            else
+                                setErrorCompanyName("")
+                        }}
+                        id="companyName"
+                        name="companyName"
+                        type="text"
+                        autoComplete="companyName"
+                        required
+                    />
+                    <div>{errorCompanyName}</div>
+                </div>
+                <div>
+                    <label htmlFor="firstName">
+                        First name
+                    </label>
+                    <label>*</label>
+                    <input
+                        onChange={(e) => {
+                            setFirstName(e.target.value)
+                            if (!RegExp("^[a-zA-Z0-9!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~]{1,20}$").test(firstName))
+                                setErrorFirstName("Incorrect First Name")
+                            else
+                                setErrorFirstName("")
+                        }}
+                        id="firstName"
+                        name="firstName"
+                        type="text"
+                        autoComplete="firstName"
+                        required
+                    />
+                    <div>{errorFirstName}</div>
+                </div>
+                <div>
+                    <label htmlFor="LastName">
+                        Last name
+                    </label>
+                    <label>*</label>
+                    <input
+                        onChange={(e) => {
+                            setLastName(e.target.value)
+                            if (!RegExp("^[a-zA-Z0-9!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~]{1,20}$").test(LastName))
+                                setErrorLastName("Incorrect Last Name")
+                            else
+                                setErrorLastName("")
+                        }}
+                        id="LastName"
+                        name="LastName"
+                        type="text"
+                        autoComplete="LastName"
+                        required
+                    />
+                    <div>{errorLastName}</div>
+                </div>
+                <div>
+                    <label htmlFor="phone">
+                        Phone number
+                    </label>
+                    <label>*</label>
+                    <input
+                        onChange={(e) => {
+                            setPhoneNumber(e.target.value)
+                            if (!RegExp("/^\+[0-9]{8,}$/").test(phoneNumber))
+                                setErrorPhoneNumber("Incorrect Phone Number")
+                            else
+                                setErrorPhoneNumber("")
+                        }}
+                        id="phoneNumber"
+                        name="phoneNumber"
+                        type="tel"
+                        autoComplete="tel"
+                        required
+                    />
+                    <div>{errorPhoneNumber}</div>
+                </div>
+                <div>
+                    <label htmlFor="email">
+                        Email
+                    </label>
+                    <label>*</label>
+                    <input onChange={(e) => {
+                        setEmail(e.target.value)
+                        if (!RegExp("^[\\w\\.-]+@[\\w\\.-]+\\.[\\w\\.-]+$").test(email)) {
+                            setErrorEmail("Incorrect Email");
+                        } else
+                            setErrorEmail("")
+                    }}
+                           id="email"
+                           name="email"
+                           type="email"
+                           autoComplete="email"
+                           required
+                    />
+                    <div>{errorEmail}</div>
+                </div>
+
+                <div>
+                    <label htmlFor="password">
+                        Password
+                    </label>
+                    <label>*</label>
+                    <input
+                        onChange={(e) => {
+                            setPassword1(e.target.value)
+                            if (CheckPassword(password1).code == 0)
+                                setErrorPassword1("")
+                        }}
+                        id="password"
+                        name="password"
+                        type={showPassword1 ? 'text' : 'password'}
+                        autoComplete="current-password"
+                        required
+                    />
+                    <button
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setShowPassword1(!showPassword1)
+                        }}>
+                        {showPassword1 ?
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                 stroke="black" className=" w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"/>
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                            </svg>
+                            : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                   strokeWidth="1.5" stroke="black" className=" w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"/>
+                            </svg>
+                        }
+                    </button>
+                    <div>{errorPassword1}</div>
+                </div>
+                <div>
+                    <label htmlFor="password">
+                        Confirm password
+                    </label>
+                    <label>*</label>
+                    <input
+                        onChange={(e) => {
+                            setPassword2(e.target.value)
+                            if (password1 == password2)
+                                setErrorPassword2("")
+                        }}
+                        id="password-repeat"
+                        name="password-repeat"
+                        type={showPassword2 ? 'text' : 'password'}
+                        autoComplete="current-password"
+                        required
+                    />
+                    <button
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setShowPassword2(!showPassword2)
+                        }}>
+                        {showPassword2 ?
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                 stroke="black">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"/>
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                            </svg>
+                            : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                   strokeWidth="1.5" stroke="black">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"/>
+                            </svg>
+                        }
+                    </button>
+                    <div>{errorPassword2}</div>
+                </div>
+                <button type="submit">
+                    Confirm
+                </button>
+            </form>
+            <div>
+                <label>Do you have an account?</label>
+                <a href={"/login"}>
+                    Log in
+                </a>
+            </div>
+        </div>
+        <p>Footer</p>
+        </body>
+    )
 }
 
 export default Registration;
