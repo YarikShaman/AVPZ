71e0cd801850976593cd93c8e12ecaad277da256
Yarik_Shaman <myarik01@gmail.com>
2023-10-30 17:37:32 +0200

    feat/main_page(+base architecture of main page)

---
+++ avpz/src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './Header.module.css';
+
+export function Header(){
+    return(
+        <div className={styles.background}>
+
+        </div>
+    )
+}
